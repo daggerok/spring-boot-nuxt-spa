24ffe150b03f1b4bd36d55cd39cd254249de04f2
Maksim Kostromin <daggerok@gmail.com>
2018-12-24 14:03:43 +0200

    Update versions.

--- src/main/java/com/github/daggerok/SpringBootNuxtSpaApplication.java
+++ src/main/java/com/github/daggerok/SpringBootNuxtSpaApplication.java
@@ -1,4 +1,4 @@
-package daggerok;
+package com.github.daggerok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/daggerok/SpringBootNuxtSpaApplicationTests.java
+++ src/test/java/com/github/daggerok/SpringBootNuxtSpaApplicationTests.java
@@ -1,16 +1,16 @@
-package daggerok;
+package com.github.daggerok;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class SpringBootNuxtSpaApplicationTests {
 
     @Test
-    public void contextLoads() {
-    }
-
+    public void contextLoads() { }
 }
